d1f9cabaaab371377f9cbe54796408fec3e9c54b
vincent van Beek <vin.van.beek@outlook.com>
2016-11-25 20:47:21 +0100

    agenda component added

---
+++ src/app/agenda.component.ts
@@ -0,0 +1,9 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'agendaComponent',
+  templateUrl: 'agenda.component.html'
+})
+export class AgendaComponent {
+   agendaName = 'agenda Julien';
+}
